ee5978eae9f9a1e1c5c98d0600629b4c4022e302
SavioVitorAlves <saviovitor2@gmail.com>
2024-02-24 10:36:17 -0300

    codificação em index.js

---
+++ Projeto Lider/index.js
@@ -0,0 +1,73 @@
+/*botao 01*/
+    document.getElementById("btn-01").addEventListener("click", function() {
+        var button = document.getElementById("btn-01");
+        var randomColor = '#fff' ; // Gera uma cor aleatória em hexadecimal
+        button.style.backgroundColor = 'rgb(255, 152, 55)';
+    });
+
+    document.getElementById("btn-01").addEventListener("click", function(){
+    var button = document.getElementById("btn-02");
+        button.style.backgroundColor = 'rgb(221, 207, 187)'
+    });
+    document.getElementById("btn-01").addEventListener("click", function(){
+        var button = document.getElementById("btn-03");
+            button.style.backgroundColor = 'rgb(221, 207, 187)'
+    });
+    document.getElementById("btn-01").addEventListener("click", function(){
+        var button = document.getElementById("btn-04");
+            button.style.backgroundColor = 'rgb(221, 207, 187)'
+    });
+/*botao 02*/
+    document.getElementById("btn-02").addEventListener("click", function() {
+        var button = document.getElementById("btn-02");
+        var randomColor = '#fff' ; // Gera uma cor aleatória em hexadecimal
+        button.style.backgroundColor = 'rgb(255, 152, 55)';
+    });
+    document.getElementById("btn-02").addEventListener("click", function(){
+        var button = document.getElementById("btn-01");
+        button.style.backgroundColor = 'rgb(221, 207, 187)'
+    });
+    document.getElementById("btn-02").addEventListener("click", function(){
+        var button = document.getElementById("btn-03");
+        button.style.backgroundColor = 'rgb(221, 207, 187)'
+    });
+    document.getElementById("btn-02").addEventListener("click", function(){
+        var button = document.getElementById("btn-04");
+        button.style.backgroundColor = 'rgb(221, 207, 187)'
+    });
+/*botao 03*/
+    document.getElementById("btn-03").addEventListener("click", function() {
+        var button = document.getElementById("btn-03");
+        var randomColor = '#fff' ; // Gera uma cor aleatória em hexadecimal
+        button.style.backgroundColor = 'rgb(255, 152, 55)';
+    });
+    document.getElementById("btn-03").addEventListener("click", function(){
+        var button = document.getElementById("btn-01");
+        button.style.backgroundColor = 'rgb(221, 207, 187)'
+    });
+    document.getElementById("btn-03").addEventListener("click", function(){
+        var button = document.getElementById("btn-02");
+        button.style.backgroundColor = 'rgb(221, 207, 187)'
+    });
+    document.getElementById("btn-03").addEventListener("click", function(){
+        var button = document.getElementById("btn-04");
+        button.style.backgroundColor = 'rgb(221, 207, 187)'
+    });
+/*botao 04*/
+    document.getElementById("btn-04").addEventListener("click", function() {
+        var button = document.getElementById("btn-04");
+        var randomColor = '#fff' ; // Gera uma cor aleatória em hexadecimal
+        button.style.backgroundColor = 'rgb(255, 152, 55)';
+    });
+    document.getElementById("btn-04").addEventListener("click", function(){
+        var button = document.getElementById("btn-01");
+        button.style.backgroundColor = 'rgb(221, 207, 187)'
+    });
+    document.getElementById("btn-04").addEventListener("click", function(){
+        var button = document.getElementById("btn-02");
+        button.style.backgroundColor = 'rgb(221, 207, 187)'
+    });
+    document.getElementById("btn-04").addEventListener("click", function(){
+        var button = document.getElementById("btn-03");
+        button.style.backgroundColor = 'rgb(221, 207, 187)'
+    });
